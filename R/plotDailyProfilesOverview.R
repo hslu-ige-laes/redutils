d0b018f1bfbf0e5efe7e45879267b6d9d5983bae
Reto Marek <reto.marek@hslu.ch>
2020-11-23 14:25:02 +0100

    aligned naming of function arguments

--- R/plotDailyProfilesOverview.R
+++ R/plotDailyProfilesOverview.R
@@ -1,17 +1,17 @@
 plotDailyProfilesOverview <- function(data,
                                       locTimeZone = "UTC",
-                                      titlePlot = "Daily Profiles Overview by Weekday and Season",
-                                      titleYAxis = "Energy Consumption (kWh/h)",
-                                      lineColor = "black",
+                                      main = "Daily Profiles Overview by Weekday and Season",
+                                      ylab = "Energy Consumption (kWh/h)",
+                                      col = "black",
                                       confidence = 95.0){
   #' Plot Daily Profiles Overview
   #'
   #' Plot a Graph with Daily Energy Consumption Profiles by Weekday and Season
   #' @param data Dataset to use for plot, minimum 1 hour aggregated. Must be a data.frame with "timestamp YmdHMS, energy consumption"
   #' @param locTimeZone Time zone of timestamp, default "UTC"
-  #' @param titlePlot Main title of plot, default "Before/After Optimization"
-  #' @param titleYAxis y-axis title, default "Energy Consumption (kWh/month)"
-  #' @param lineColor Line color of median value, default "black"
+  #' @param main Main title of plot, default "Before/After Optimization"
+  #' @param ylab y-axis title, default "Energy Consumption (kWh/month)"
+  #' @param col Line colour of median value, default "black"
   #' @param confidence Confidence interval for upper ribbon in percent (lower is calculated automatically), default 95 percent"
   #'
   #' @return Returns a ggplot object
@@ -27,9 +27,9 @@ plotDailyProfilesOverview <- function(data,
 
   # function argument checks
   checkmate::assertString(locTimeZone)
-  checkmate::assertString(titlePlot)
-  checkmate::assertString(titleYAxis)
-  checkmate::assertString(lineColor)
+  checkmate::assertString(main)
+  checkmate::assertString(ylab)
+  checkmate::assertString(col)
   checkmate::assertNumber(confidence, lower = 50.0, upper = 100.0)
 
 
@@ -69,14 +69,14 @@ plotDailyProfilesOverview <- function(data,
 
   plot <- ggplot2::ggplot(df.h) +
     ggplot2::geom_ribbon(ggplot2::aes(x = dayhour, ymin = valueLower, ymax = valueUpper), fill = "darkgrey", alpha = 0.7) +
-    ggplot2::geom_line(ggplot2::aes(x = dayhour, y = valueMedian), color = lineColor, alpha = 0.5) +
-    ggplot2::labs(x = "\nHour of day", y = paste0(titleYAxis, "\n")) +
+    ggplot2::geom_line(ggplot2::aes(x = dayhour, y = valueMedian), color = col, alpha = 0.5) +
+    ggplot2::labs(x = "\nHour of day", y = paste0(ylab, "\n")) +
     ggplot2::facet_grid(season~weekday) +
     ggplot2::theme_minimal() +
     ggplot2::theme(axis.text.x = ggplot2::element_text(colour = "grey50", size = 8, hjust = 0.5, vjust = 0.5, face = "plain")) +
     ggplot2::theme(axis.text.y.right = ggplot2::element_text(angle = 180)) +
     ggplot2::scale_x_continuous(breaks = seq(from = 0, to = 23, by = 6)) +
-    ggplot2::ggtitle(paste0(titlePlot, "\n"))
+    ggplot2::ggtitle(paste0(main, "\n"))
 
   return(plot)
 
