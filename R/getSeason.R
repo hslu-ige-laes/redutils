9e817e21ca527d3c52570554c6c53464ff911fb5
Reto Marek <reto.marek@hslu.ch>
2020-11-23 14:24:52 +0100

    aligned naming of function arguments

--- R/getSeason.R
+++ R/getSeason.R
@@ -1,9 +1,9 @@
-getSeason <- function(x, seasonNames = c("Winter","Spring","Summer","Fall")){
+getSeason <- function(x, seasonlab = c("Winter","Spring","Summer","Fall")){
   #' Get season from date
   #'
   #' Get the season name out of a date
   #' @param x a POSIXct, POSIXlt, Date, chron, yearmon, yearqtr, zoo, zooreg, timeDate, xts, its, ti, jul, timeSeries, or fts object..
-  #' @param seasonNames concatenated strings with 4 seasons, default c("Winter","Spring","Summer","Fall")
+  #' @param seasonlab Season labels, concatenated strings with 4 seasons, default c("Winter","Spring","Summer","Fall")
   #'
   #' @return Returns the corresponding season.
   #' @importFrom checkmate assertCharacter
@@ -13,17 +13,17 @@ getSeason <- function(x, seasonNames = c("Winter","Spring","Summer","Fall")){
   #' getSeason(x) #"Spring"
   #'
   #' x <- as.Date("2019-04-01")
-  #' getSeason(x, seasonNames = c("Winter","Frühling","Sommer","Herbst")) #"Frühling"
+  #' getSeason(x, seasonlab = c("Winter","Frühling","Sommer","Herbst")) #"Frühling"
 
   # function argument checks
-  checkmate::assertCharacter(seasonNames, min.chars = 1, len = 4)
+  checkmate::assertCharacter(seasonlab, min.chars = 1, len = 4)
 
   # function code
   numeric.date <- 100 * lubridate::month(x) + lubridate::day(x)
 
   cuts <- base::cut(numeric.date, breaks = c(0,0229,0531,0831,1130,1231))
 
-  levels(cuts) <- c(seasonNames[1], seasonNames[2], seasonNames[3], seasonNames[4], seasonNames[1])
+  levels(cuts) <- c(seasonlab[1], seasonlab[2], seasonlab[3], seasonlab[4], seasonlab[1])
 
   return(as.character(cuts))
 }
